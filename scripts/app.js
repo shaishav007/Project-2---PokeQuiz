7e7d53e3585eba0a3f4943a4d475f5f2de3678b1
Colm O'Sullivan <colm.osullivan1@gmail.com>
2022-10-04 20:34:06 -0400

    small changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,62 +1,80 @@
-
-// const pokeApp ={};
-
-// const pokeArray = [];
-//       for (i = 1; i <= 4; i++) {
-//         //generate 1 random number
-//         const num = Math.ceil(Math.random() * 150);
-//         // push these to the pokeArray array.
-//         pokeArray.push(num);
-//       }
-
-
-
-// pokeApp.init = function(){
-//     // query the pokemone API
-//     pokeApp.getName();
-//     pokeApp.getSprite();
-// }
-
-// pokeApp.init();
-
 // we're going to prompt the user with a question 4 options to choose from.
     // dummy question and 4 dummy answers
     // one has to be correct, others have to be incorrect.
     // dummy array: 4 objects
     // make another random generator with value 0-3
-    a=[55,66,77,88,];
-    correctAnswer= Math.floor(Math.random() * 4);
-    console.log(a[correctAnswer]);
-    console.log(correctAnswer);
+    const pokeApp ={};
+    pokeApp.pokeArray = new Array();
+    // we need to populate pokeArray with objects that have a name and image associated with them.
+    pokeApp.saveNameAndImage = function (data) {
+        // add pokemon object to pokeArray
+        pokeApp.pokeArray.push({
+            "name": data.name,
+            "image": data.sprites.front_default,
+            // by default it should be the wrong answer.
+            // we will generate one correct answer and set the below property to true. Leaving ONE correct answer.
+            "isCorrect":false
+        });
+        // const name = data.name;
+        
+      };
+    pokeApp.init = function(){
+        // first, we fetch pokemon and populate the elements
+        pokeApp.populate();
+        
+    };
 
-    const question = document.createElement("p");
-    question.textContent="pokemon?"
-
+      pokeApp.fillMarkups = function (){
+        // first we have to fill 4 buttons with 4 pokemon names, then add them to ul.Then figure out which one is right and then display the image of that pokemon. After this is set up, we set up event listeners.
+        // option button is a new button that we've created.
+        // const optionButton = document.createElement('button');
+        // // we will have to get the right pokemon from pokeArray.
+        // optionButton.innerText=pokeApp.pokeArray[0].name;
+        // console.log(optionButton)
+        // trying a forEach approach here. forEach pokemon in pokeArray generate one button with thier name.
     
+    //    pokeApp.pokeArray =  Array.from(pokeApp.pokeArray)
+    //    console.log(typeof(pokeApp.pokeArray))
+        pokeApp.pokeArray.forEach((item) => {
+            console.log(item);
+        });
+        // console.log(pokeApp.pokeArray);
+        // console.log(pokeApp);
+      };
+      
+    // writing the function to populate the image and four options.
+    pokeApp.populate = function(){
+        const indexes = [];
+      for (i = 1; i <= 4; i++) {
+        //generate 1 random number
+        const num = Math.ceil(Math.random() * 150);
+        // push these to the pokeArray array.
+        indexes.push(num);
+      }
+        for (i = 1; i <= 4; i++) {
+            // putting the API URL into a variable that can change based on the numbers we feed to it from the array.
+            let pokeURL = "https://pokeapi.co/api/v2/pokemon/";
+            // we feed the array to the url, this adds a number at the end of the url (example:https://pokeapi.co/api/v2/pokemon/150 fetchs all info it has on Metwo). 
+            pokeURL += indexes.pop();
+            fetch(pokeURL)
+              .then((response) => {
+                return response.json();
+                //at this stage, the loop is returning the data that we can more easily parse through, and returning specific data on the four numbers from he array. To continue from the earlier example, we can now see that 150 returns Mewtwo and all associated info of that pokemon.
+              })
+              .then((data) => {
+                // call the name extractor function here
+                pokeApp.saveNameAndImage(data);
+                // the function below has the code to fill the buttons and the image tag.
+              });
+              pokeApp.fillMarkups(pokeApp.pokeArray);
+          }
+        
+    };
+    
+
+    pokeApp.init();
 
     // buttons
-    const buttonOne = document.createElement("button")
-    buttonOne.textContent= a[0];
-
-    const buttonTwo = document.createElement("button")
-    buttonTwo.textContent=a[1];
-
-    const buttonThree = document.createElement("button")
-    buttonThree.textContent= a[2];
-
-    const buttonFour = document.createElement("button")
-    buttonFour.textContent=a[3];
-
-    // On click of button, check if value === correctAnswer
-    buttonOne.addEventListener('click',function(){
-        console.log(this.textContent);
-    })
-    
-    const gameSection = document.querySelector('.buttonBoard');
-    gameSection.appendChild(buttonOne);
-    gameSection.appendChild(buttonTwo);
-    gameSection.appendChild(buttonThree);
-    gameSection.appendChild(buttonFour);
     // based on our correctAnswer, we append to the DOM an image and a name to one button, we also
     // append the remaining 3 names to our buttons from the array.
 
